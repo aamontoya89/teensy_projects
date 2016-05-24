128da0087f8fa5423b99f22ed6b1718f28f996b3
aaron <aamontoya@gmail.com>
2016-05-24 00:36:00 -0400

    multiple teensy arduino

--- 05_multiple_teensys/multiple_teensys/multiple_teensys.ino
+++ 05_multiple_teensys/multiple_teensys/multiple_teensys.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+//multiple teensys
 
+//midi channel
+int sendChannel = 1;
+
+//midi control number
+int sendControl = 9;
+
+//midi control value
+int sendValue = 120;
+
+void setup() {
+ //begin serial communication
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //usbMIDI.sendControlChange(control, value, channel);
+  usbMIDI.sendControlChange(sendControl, sendValue, sendChannel);
 
+  delay(1000);
 }
